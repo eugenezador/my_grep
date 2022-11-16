7c250eb8b0342d89219986c96fdb1d7cc72babbb
eugenezador <eugenezador@gmail.com>
2022-11-16 16:29:03 +0300

    update

--- ex_grep.c
+++ ex_grep.c
@@ -3,6 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+// typedef struct{
+//     char name[100];
+// }file_s;
+
+// typedef struct {
+//     char name[100];
+// }pattern_s;
+
+
+
 typedef struct {
     char patterns[10][40];
     char files[10][40];
@@ -18,13 +29,25 @@ typedef struct {
 } grep_flags;
 
 
+// void count_parse_data(int argc, char** argv, int* file_counter, int* pattern_counter);
 void parse_input(int argc, char** argv, grep_flags* flags, grep_data* data);
 void parse_flags(grep_flags* flags, char* flag_array);
 
 
 
 int main(int argc, char** argv) {
+
+    // int file_counter = 0;
+    // int pattern_counter = 0;
+
+    // count_parse_data(argc, argv, &file_counter, &pattern_counter);
+
     grep_flags flags = {0, 0, 0, 0, 0, 0};
+    
+    // file_s input_files[file_counter];
+
+    // pattern_s input_patterns[pattern_counter];
+
     grep_data data = {{""}, {""}};
     
     parse_input(argc, argv, &flags, &data);
@@ -41,16 +64,20 @@ void parse_input(int argc, char**argv, grep_flags* flags, grep_data* data) {
     int files_counter = 0;
     
     for(int i = 1; i < argc; i++) {
+
+        // is e pattern
         if(argv[i][0] == '-' && argv[i + 1][0] != '-' && strchr(argv[i], 'e') && i != argc - 1) {
             strcpy(data->patterns[pattern_counter], argv[i + 1]);
             pattern_counter++;
             e_flag_counter++;
             flags->e = true;
         }
-        if(argv[i][0] != '-' && !strstr(argv[i - 1], "-e")) {
+
+        // is file
+        if(argv[i][0] != '-' && strspn(argv[i - 1], "-e") < 2 && pattern_counter != 0) {
             strcpy(data->files[files_counter], argv[i]);
             files_counter++;
-            printf("%d\n", files_counter);
+            printf("%d - %s\n", i, data->files[files_counter]);
         }
     }
     
